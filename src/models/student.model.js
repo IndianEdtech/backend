a5089ddca23eec75f70d4c966ddf94f670f4ef20
Waseem Ahemed <78431077+waseemahemed@users.noreply.github.com>
2025-05-03 11:48:33 +0530

    create student model

---
+++ src/models/student.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const studentSchema = new Schema({
+    studentName : {
+        type:String,
+        required:true,
+    },
+    studentClass : {
+        type:String,
+        required:true,
+    },
+    dob:{
+        type:String,
+        required:true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowecase: true,
+    },
+    mobileNo:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    Address:{
+        type:String,
+        required:true,
+    }
+},{
+    timestamps:true
+});
+
+export const User = mongoose.model("Student", studentSchema);
